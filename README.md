56ad902346953f9a33cf852d51f9463022de6992
Andrey <karpovich.andrey@gmail.com>
2024-12-05 18:23:58 +0200

    Initial app upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Expenses Tracker Demo Angular18
+
+This demo project of expences-incomes tracker for possible Galaxy traveller (thanks to Douglas Adams).
+Tech task in brief:
+- The user should be able to add expenses and incomes, view the overall balance, and filter records by categories.
+- The user can add records for income and expenses through a form (Name, Amount, Type, Category, Date)
+- All data must be stored in localStorage so that records persist after a page reload.
+- Angular latest version
+- Readable and understandable names of variables, classes, functions and methods
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule} from '@angular/material/expansion';
+
+import { RecordsService } from './records.service';
+import { AddRecordComponent } from './add-record/add-record.component';
+import { LogExpenseComponent } from './records-history/records-history.component';
+import { BasicCategories, Record } from './shared/constants';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NgIf, AddRecordComponent, LogExpenseComponent, AsyncPipe, MatGridListModule, MatButtonModule, MatExpansionModule, 
+    MatCardModule, MatIconModule, MatFormFieldModule, MatChipsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  expType = Record.Exp;
+  incType = Record.Inc;
+  expCategories = BasicCategories[this.expType];
+  incCategories = BasicCategories[this.incType];
+  recordsService = inject(RecordsService);
+  $expensesSum = this.recordsService.expensesSumSignal;
+  $incomesSum = this.recordsService.incomesSumSignal;
+  $currentBalance = this.recordsService.currentBalance;
+
+  ngOnInit() {
+    this.recordsService.initData();    
+  }
+}
